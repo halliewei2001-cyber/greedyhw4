22998068eee5c1f0626dbdaa27eb28d134d52048
halliewei2001-cyber <halliewei2001@gmail.com>
2025-11-21 04:59:13 -0500

    modified:   HW4P1_Student_Notebook.ipynb 	modified:   mytorch/nn/activation.py

--- mytorch/nn/activation.py
+++ mytorch/nn/activation.py
@@ -20,11 +20,16 @@ class Softmax:
         if self.dim > len(Z.shape) or self.dim < -len(Z.shape):
             raise ValueError("Dimension to apply softmax to is greater than the number of dimensions in Z")
         
+        max_z, _ = torch.max(Z, dim=self.dim, keepdim=True)
+        Z_shifted = Z - max_z
+
+        exp_z = torch.exp(Z_shifted)
+        sum_exp = torch.sum(exp_z, dim=self.dim, keepdim=True)
         # TODO: Implement forward pass
         # Compute the softmax in a numerically stable way
         # Apply it to the dimension specified by the `dim` parameter
-        self.A = NotImplementedError
-        raise NotImplementedError
+        self.A = exp_z / sum_exp
+        return self.A
 
     def backward(self, dLdA):
         """
@@ -40,16 +45,30 @@ class Softmax:
            
         # Reshape input to 2D
         if len(shape) > 2:
-            self.A = NotImplementedError
-            dLdA = NotImplementedError
+            dims = list(range(len(shape)))
+            dims.remove(self.dim)
+            dims.append(self.dim)
+
+            A_2d = self.A.permute(*dims).reshape(-1, C)
+            dLdA_2d = dLdA.permute(*dims).reshape(-1, C)
+
+            self.A = A_2d
+            dLdA = dLdA_2d
+
+        inner = (dLdA * self.A).sum(dim=1, keepdim=True)
+        dLdZ_2d = self.A * (dLdA - inner)
+        dLdZ = dLdZ_2d.reshape(*shape)
 
         # Reshape back to original dimensions if necessary
         if len(shape) > 2:
             # Restore shapes to original
-            self.A = NotImplementedError
-            dLdZ = NotImplementedError
+            dLdZ = dLdZ_2d.reshape(*(shape[d] for d in dims))
+        
+            inv_dims = [dims.index(i) for i in range(len(dims))]
+            self.A = self.A.reshape(*(shape[d] for d in dims)).permute(*inv_dims)
+            dLdZ = dLdZ.permute(*inv_dims)
 
-        raise NotImplementedError
+        return dLdZ
  
 
     
