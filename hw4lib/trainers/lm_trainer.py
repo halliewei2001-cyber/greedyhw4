ad082c32a1f9342f174f39dfd27bda44f1365daa
halliewei2001-cyber <halliewei2001@gmail.com>
2025-11-21 20:14:28 -0500

    modified:   hw4lib/trainers/lm_trainer.py

--- hw4lib/trainers/lm_trainer.py
+++ hw4lib/trainers/lm_trainer.py
@@ -400,8 +400,11 @@ class LMTrainer(BaseTrainer):
             else:
                 # TODO: Use the prompts and the generate_greedy method you implemented in the SequenceGenerator class to generate sequences
                 print("Generating with greedy search...")
-                seqs, scores = NotImplementedError, NotImplementedError
-                raise NotImplementedError # Remove if you implemented the greedy search method
+                seqs, scores = generator.generate_greedy(prompts,
+                    temperature=generation_config.get('temperature', 1.0),
+                    repeat_penalty=generation_config.get('repeat_penalty', 1.0),
+                )
+                 # Remove if you implemented the greedy search method
 
         # Post-process sequences (trim upto EOS token)
         processed_seqs = generator.post_process_sequence(seqs, self.tokenizer)
